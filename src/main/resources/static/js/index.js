df0cb9a4d1f310ba7bc4055c7e3200bba000be24
涂维 <44657440+tuwei1314@users.noreply.github.com>
2023-03-27 21:56:47 +0800

    Update index.js

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -7,7 +7,7 @@ $(function(){
 function adduser(){
 	layer.open({
 		  id :"addUser",
-		  title :"添加美女",
+		  title :"添加历史人物",
 		  type: 2, 
 		  content: 'addUserPage', //这里content是一个URL，如果你不想让iframe出现滚动条，你还可以content: ['${contextPath}/s/loadProdListPage', 'no']
 		  area: ['400px', '280px']
@@ -17,7 +17,7 @@ function adduser(){
 
 function queryuserList(){
 	layer.open({
-		  title :"今晚翻盘哪个",
+		  title :"今晚想穿越到哪个人物所处的时代",
 		  type: 2, 
 		  content: 'queryUserListPage', //这里content是一个URL，如果你不想让iframe出现滚动条，你还可以content: ['${contextPath}/s/loadProdListPage', 'no']
 		  area: ['1024px', '650px']
